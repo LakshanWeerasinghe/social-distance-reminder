ab92cd9c228fb91e8552e771e80bcea914a7529b
Lakshan Weerasinghe <lak98weerasinghe@gmail.com>
2021-12-21 22:57:24 +0530

    deviceUUID saving firebase function

--- FirebaseCloudMessaging/functions/index.js
+++ FirebaseCloudMessaging/functions/index.js
@@ -1,5 +1,9 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require('express');
+const bodyParser = require('body-parser');
+const generateUUID = require('./util');
+
 admin.initializeApp();
 
 exports.covidContactNotification = functions.firestore.document("CovidContact/{docId}").onCreate(
@@ -35,3 +39,44 @@ exports.covidContactNotification = functions.firestore.document("CovidContact/{d
         )
     }
 );
+
+
+
+// Intialize Express server
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+//initialize the database and the collection 
+const db = admin.firestore();
+const userDetailsCollection = 'userDetails';
+
+exports.webApi = functions.https.onRequest(main);
+
+app.post('/generate-deviceId', async (req, res) => {
+
+    try {
+
+        const deviceUUID = generateUUID.generateUUID();
+        const userId = req.body['userId'];
+        const fcmToken = req.body['fcmToken'];
+
+        
+        const newDoc = await db.collection(userDetailsCollection).doc(userId).set({
+            deviceUUID,
+            fcmToken
+        });
+
+        deviceDetails = {
+            deviceUUID
+        };
+
+        res.status(201).send(deviceDetails);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+})
